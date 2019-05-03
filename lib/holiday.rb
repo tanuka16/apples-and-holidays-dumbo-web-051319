6bf309b007e0d3439785917efecea98bff45967f
Tanuka Das <das.tanuka@yahoo.com>
2019-05-03 22:37:19 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 
 end
 
@@ -31,19 +34,22 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
+#flatten : Returns a new array that is a one-dimensional flattening of self (recursively). That is, for every element that is an array, extract its elements into the new array.
+#values: Returns a new array populated with the values from hsh.
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -54,17 +60,33 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+
+    holidays.each do |holiday, items|
+      holiday_string_split = holiday.to_s.split("_").collect do |word|
+        words = word.capitalize!
+
+      end.join(" ")
+
+      puts "  #{holiday_string_split}: #{items.join(", ")}"
+
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
 
+  holiday_hash.each do |seasons, holidays|        #go through the holiday_hash's list of seasons (key), whichh has the values od holidays
+    holidays.each do |holiday, supply|            #now go through each holiday and go throught its supply
+      if supply.include? ("BBQ")                  #check if any of the supply list includes "BBQ"
+        bbq_holidays << holiday                   #if it does, place those holiday names in the empty BBQ_holidays array
+        bbq_holidays.flatten                      #every element that is an array, extract its elements into the new array
+      end
+    end
+  end
+  bbq_holidays
 end
-
-
-
-
-
-
-
